d48d3ec82d6fe2d5da02405e58aaada2adf0dca3
expressmailing <prestashop@axalone.fr>
2016-05-03 14:15:06 +0200

    Upgrade file

---
+++ upgrade/Upgrade-1.2.0.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 2014-2015 (c) Axalone France - Express-Mailing
+ *
+ * This file is a commercial module for Prestashop
+ * Do not edit or add to this file if you wish to upgrade PrestaShop or
+ * customize PrestaShop for your needs please refer to
+ * http://www.express-mailing.com for more information.
+ *
+ * @author    Axalone France <info@express-mailing.com>
+ * @copyright 2014-2015 (c) Axalone France
+ * @license   http://opensource.org/licenses/GPL-3.0  GNU General Public License, version 3 (GPL-3.0)
+ */
+
+function upgrade_module_1_2_0($module)
+{
+	return $module->installDB(true);
+}
